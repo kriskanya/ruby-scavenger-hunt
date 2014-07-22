84602e9f0ac82ef1f71abd12d2a9f146f2f7825b
Kristian Kanya <kriskanya@gmail.com>
2014-07-22 12:11:10 -0500

    hopefully works

--- lib/my_vector.rb
+++ lib/my_vector.rb
@@ -1,13 +1,35 @@
-class MyVector
+require 'matrix'
 
-  def initialize (x, y)
-    @x = x
-    @y = y
+class MyVector < Vector
+
+  # you don't need an initialize class because if you wrote one you'd be overwriting Vector
+
+  # def calculate_distance(vector1, vector2)
+  #   distance = vector1 - vector2
+  #   # distance = Math.sqrt((vector1.x - vector2.x)** + (vector1.y - vector2.y)**)
+  #   distance.magnitude
+  # end
+
+
+  # Jurnell's code:
+  def distance(v)
+    Math.sqrt((self[0] - v[0])**2 + (self[1] - v[1])**2)
   end
 
-  def calculate_distance(vector1, vector2)
-    distance = Math.sqrt((vector1.x - vector2.x)**2 + (vector1.y - vector2.y)**2)
-    distance
-  end
+  # # how you would call this
+  # MyVector[1,2].distance(Vector[2,2])
+  #
+  # the MyVector[1,2] part of this (which corresponds to 'self') is like 'this' in Javascript
 
+  def self.pathlength(*n)
+    # how would you do it for two points?
+    # MyVector[1,2].distance(Vector[2,2])
+
+    # n.inject(0) { |sum, number| sum + number}
+
+    total_distance = 0
+    n.each do |i|
+      total_distance += MyVector[n[i], n[i]].distance(Vector[n[i], n[i]])
+  end
+  total_distance
 end
